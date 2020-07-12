82798b58a6ec7bd70016fba77a2d075d1596aef5
koreanjones <charles.justin.jones@gmail.com>
2020-07-11 22:51:15 -0700

    add form input component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 
 function App() {
   return (
     <div className='App'>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
+      <Route exact path='/signin' component={SignInSignUpPage}/>
     </Switch>
     </div>
   );
